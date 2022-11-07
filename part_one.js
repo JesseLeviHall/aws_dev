dd321e50217d139fcf48ea209981b29cd7cad422
jesse h <jesselevihall@gmail.com>
2022-11-06 17:26:05 -0700

    IAM accounts

--- part_one.js
+++ part_one.js
@@ -11,9 +11,7 @@ The labs repo has mini projects that I should practice with. Pet Cuddle-o-tron i
 
 The aws console is the place to go to manage your aws services.
 
-Animals for life rescue organization, IoT and big data. 
+Animals for life rescue organization, IoT and big data. Animal care, activists and lobbyists, as distributed business scenario that utilzes cloud architecture 
 
-Animal care, activists and lobbyists, as distributed business scenario that utilzes cloud architecture 
-
-AWS is a cloud service provider.
+A company may use many aws accounts with emails and passwords.. each account is like a container, the account owner cannot limit the permissions for itself, but AIM accounts can be added to them with various permissions. 
 */
