b24730fa562c98ad9c9076a77791b6a7cd660178
jesse h <jesselevihall@gmail.com>
2022-11-06 17:30:55 -0700

    cloud service

--- part_one.js
+++ part_one.js
@@ -13,5 +13,7 @@ The aws console is the place to go to manage your aws services.
 
 Animals for life rescue organization, IoT and big data. Animal care, activists and lobbyists, as distributed business scenario that utilzes cloud architecture 
 
-A company may use many aws accounts with emails and passwords.. each account is like a container, the account owner cannot limit the permissions for itself, but AIM accounts can be added to them with various permissions. 
+A company may use many aws accounts with emails and passwords.. each account is like a container, the account owner cannot limit the permissions for itself, but AIM accounts can be added to them with various permissions.
+
+IAM is the service that manages users and permissions.
 */
