bbc9678b606b8a5aa0cbf5e50639c43e29034705
jesse h <jesselevihall@gmail.com>
2022-11-15 19:19:27 -0700

    steganography

---
+++ encryption.js
@@ -0,0 +1,28 @@
+/* 
+
+Approaches to encryption:
+-at rest: 
+    -encrypt the data before it is stored in the database. 
+    
+
+-in transit
+    -encrypt the data before it is sent over the network. its like a secret tunnel that wraps the data. 
+
+symmetric encryption means:
+- same key for encryption and decryption. good for local security, not transference. 
+
+asymmetric encryption means:
+- two keys, one for encryption and one for decryption.
+
+
+
+Key Concepts: 
+-plaintext: 
+-ciphertext: the encrypted text, takes the key, generates plaintext
+-key: the secret code that is used to encrypt and decrypt the data.
+-encryption algorithm: eg blowfish, AES, RC4...
+-steganography: hiding data in a pattern decipherable fom a key. 
+
+
+
+*/
