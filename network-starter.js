3c9425fa6cfc0d0127040d7cc614980ad9d4e473
jesse h <jesselevihall@gmail.com>
2022-11-15 19:37:53 -0700

    OSI 7 Layer Model

---
+++ network-starter.js
@@ -0,0 +1,25 @@
+/* 
+-Local Networking = ethernet for example, or wifi
+
+-Remote Networking = internet, start and end of data transfer
+
+-Protocol = rules for data transfer
+
+-Port = a specific endpoint for data transfer
+
+-IP Address = a unique identifier for a device on a network
+
+OSI 7 Layer Model:
+1. Physical Layer
+    electrical tranmission cable or wire - copper wire. unstructured raw bit stream, no device addressing, collisions occur
+2. Data Link Layer
+    ethernet, wifi, bluetooth, etc. - structured raw bit stream, device addressing, no collisions
+3. Network Layer
+4. Transport Layer
+5. Session Layer
+6. Presentation Layer
+7. Application Layer
+
+
+
+*/
