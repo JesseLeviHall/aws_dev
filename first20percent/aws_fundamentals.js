ead1218f4c4dbda2b20831b5c86fd92aebf2325b
jesse h <jesselevihall@gmail.com>
2022-11-26 17:10:21 -0700

    public v private zone

---
+++ first20percent/aws_fundamentals.js
@@ -0,0 +1,11 @@
+/* 
+
+public vs private services from the networking perspective 
+public services are accessible from the internet
+private services are not accessible from the internet
+
+aws private zones are called vpc's virtual private clouds
+ec2 instances are vpc's that can only acced by the internet if you allow it and configure it
+
+aws public zones are called regions, they are connected to the internet but not accessible by the public internet - eg s3 bucket
+*/
