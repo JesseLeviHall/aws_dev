d9f5bb169803a0a8dcfad91f07c8015fdd52d4c6
jesse h <jesselevihall@gmail.com>
2022-12-07 20:31:01 -0700

    one policy for rall

--- s3_stuff/s3notes.ts
+++ s3_stuff/s3notes.ts
@@ -1,5 +1,7 @@
 /* 
 =============Security S3================
 S3 is private by default, so rather than identity policies, we configure access via bucket policies (resource policies).  Fortunately, the policy can allow access from outside the root account. 
+can grant anonymous access. 
+one policy per bucket
 
 */
