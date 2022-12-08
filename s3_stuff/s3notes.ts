1343461a314d7789394ec540cdd46d5cb4cddac1
jesse h <jesselevihall@gmail.com>
2022-12-07 22:36:26 -0700

    sid perameter

--- s3_stuff/s3notes.ts
+++ s3_stuff/s3notes.ts
@@ -2,6 +2,7 @@
 =============Security S3================
 S3 is private by default, so rather than identity policies, we configure access via bucket policies (resource policies).  Fortunately, the policy can allow access from outside the root account. 
 can grant anonymous access. 
-one policy per bucket
+one policy per bucket. 
+the sid is where you can add a statement ID. it is useful for debugging and troubleshooting.
 
 */
