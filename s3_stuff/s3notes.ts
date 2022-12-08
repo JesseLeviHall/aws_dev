d460a381936e6df7c1b0b60b13cac20f123a99bd
jesse h <jesselevihall@gmail.com>
2022-12-07 20:25:37 -0700

    allow/deny  anonymous

--- s3_stuff/s3notes.ts
+++ s3_stuff/s3notes.ts
@@ -1,5 +1,5 @@
 /* 
 =============Security S3================
-S3 is private by default, so rather than identity policies, we configure access via bucket policies (resource policies).
+S3 is private by default, so rather than identity policies, we configure access via bucket policies (resource policies).  Fortunately, the policy can allow access from outside the root account. 
 
 */
