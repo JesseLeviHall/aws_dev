5c36be337f42857dead4ffd2dd9a371175891f51
jesse h <jesselevihall@gmail.com>
2022-12-08 18:42:52 -0700

    anonymous resource access policy

--- s3_stuff/s3notes.ts
+++ s3_stuff/s3notes.ts
@@ -4,5 +4,8 @@ S3 is private by default, so rather than identity policies, we configure access 
 can grant anonymous access. 
 one policy per bucket. 
 the sid is where you can add a statement ID. it is useful for debugging and troubleshooting.
+In AWS, the Sid (short for "statement ID") is a unique identifier for a specific policy statement. It is used to identify a specific policy statement within a policy document. This can be useful if you need to modify or delete a specific statement within a policy. The Sid parameter is optional, but it is a good practice to include it in your policy statements to make it easier to manage your policies.
+
+typically access to a bucket from within the account is done via an identity policy, and access from outside the account is done via a bucket policy.
 
 */
