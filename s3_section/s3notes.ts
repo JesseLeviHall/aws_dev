c0c33664e97b7193d1d74d107327e2e491d994d9
jesse h <jesselevihall@gmail.com>
2022-12-11 09:55:44 -0700

    key create/delete

--- s3_section/s3notes.ts
+++ s3_section/s3notes.ts
@@ -53,6 +53,29 @@ KMS does not do the encryption or decription, you do or the service your using t
 
 KMS Keys do not leave their region. 
 
+After creating a customer managed key we entered the ShellTerminal and put: 
+echo "find all the doggos, distract them with the yumz" > battleplans.txt
+aws kms encrypt \
+    --key-id alias/catrobot \
+    --plaintext fileb://battleplans.txt \
+    --output text \
+    --query CiphertextBlob \
+    | base64 --decode > not_battleplans.enc 
+    
+aws kms decrypt \
+    --ciphertext-blob fileb://not_battleplans.enc \
+    --output text \
+    --query Plaintext | base64 --decode > decryptedplans.txt
+
+
+
+=================S3 object encryption============
+
+
+
+
+
+
 
 =================S3 lifecycle management================
 
