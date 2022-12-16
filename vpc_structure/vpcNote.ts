1b0459aad82baa6a3b6cb7ba0ebedc084074ffd0
jesse h <jesselevihall@gmail.com>
2022-12-15 18:47:38 -0700

    network access controls

--- vpc_structure/vpcNote.ts
+++ vpc_structure/vpcNote.ts
@@ -1,14 +1,5 @@
 /* 
 This lesson steps through the design choices around VPC design and IP planning.
 
-VPC Design:
-
-vpc subnets
-
-vpc routing
-
-internet gateway
-
-stateful vs stateless firewalls
 
 */
