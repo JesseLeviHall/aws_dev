bc2dfcf9db0367b79dce0b07a03910448c8484bc
jesse h <jesselevihall@gmail.com>
2022-12-18 20:23:18 -0700

    NAT Instances

--- vpc_structure/vpcNote.ts
+++ vpc_structure/vpcNote.ts
@@ -7,4 +7,7 @@ a: a virtual private cloud is a virtual network dedicated to your aws account. i
 
 q: what is a dedicated ip?
 a: a dedicated ip address is a static ip address that is associated with your aws account. you can associate a dedicated ip address with an elastic ip address or an instance in your vpc. you can also associate a dedicated ip address with an instance in your on-premises network using a hardware virtual private cloud (hvpc). dedicated ip addresses are available in the us east (n. virginia), us west (oregon), us west (n. california), eu (ireland), eu (frankfurt), asia pacific (singapore), asia pacific (sydney), asia pacific (tokyo), and south america (sao paulo) regions.
+
+q: what is a nat instance?
+a: a nat instance is a virtual machine that you launch into your public subnet. the nat instance forwards traffic from instances in your private subnet to the internet or other aws services, and then sends the response back to the instances. a nat instance must have a public ip address, and it must be associated with an elastic ip address. you can launch a nat instance using the amazon ec2 console, the aws cli, or the aws api. you can also launch a nat instance using a cloudformation template. you can launch a nat instance using a dedicated ip address. you can also launch a nat instance using a dedicated host.
 */
