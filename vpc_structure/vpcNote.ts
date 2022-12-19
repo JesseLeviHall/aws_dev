b5c4499b91e5d66b765d8bdf6b364ba5a05792cd
jesse h <jesselevihall@gmail.com>
2022-12-18 20:21:47 -0700

    dedicated ip

--- vpc_structure/vpcNote.ts
+++ vpc_structure/vpcNote.ts
@@ -1,6 +1,7 @@
 /* 
 This lesson steps through the design choices around VPC design and IP planning.
 Nat
-q: how to allow node.js permission to access uploaded files?
-a: use IAM roles
+
+q: what is a vpc?
+a: a virtual private cloud is a virtual network dedicated to your aws account. it is logically isolated from other virtual networks in the aws cloud. you can launch your aws resources, such as ec2 instances, into your vpc. you can configure your vpc to be a single tier or multi-tier architecture. you can configure your vpc to span multiple availability zones. you can also create a hardware virtual private cloud (hvpc) to connect your on-premises network to your vpc using a dedicated hardware connection.
 */
