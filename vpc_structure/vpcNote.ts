915c9ed0b8763f30ba32a4dec2a491c9fc417ba2
jesse h <jesselevihall@gmail.com>
2022-12-23 16:43:14 -0700

    Bastion v Jumpbox

--- vpc_structure/vpcNote.ts
+++ vpc_structure/vpcNote.ts
@@ -1,16 +1,13 @@
 /* 
 This lesson steps through the design choices around VPC design and IP planning.
 
-
 q: what is a vpc?
 a: a virtual private cloud is a virtual network dedicated to your aws account. it is logically isolated from other virtual networks in the aws cloud. you can launch your aws resources, such as ec2 instances, into your vpc. you can configure your vpc to be a single tier or multi-tier architecture. you can configure your vpc to span multiple availability zones. you can also create a hardware virtual private cloud (hvpc) to connect your on-premises network to your vpc using a dedicated hardware connection.
 
 vpc's are regionally isolated and regionally resilient
 
-
 q: what is a nat instance?
 a: a nat instance is a virtual machine that you launch into your public subnet. the nat instance forwards traffic from instances in your private subnet to the internet or other aws services, and then sends the response back to the instances. a nat instance must have a public ip address, and it must be associated with an elastic ip address. you can launch a nat instance using the amazon ec2 console, the aws cli, or the aws api. you can also launch a nat instance using a cloudformation template. you can launch a nat instance using a dedicated ip address. you can also launch a nat instance using a dedicated host.
-
 
 q: what is a nat gateway?
 a: a nat gateway is a managed service that makes it easy for you to connect to the internet from instances in a private subnet in your vpc.
@@ -54,13 +51,13 @@ Every subnet has 5 reserved IPs:
 DHCP stands for 
 -Dynamic Host Configuration Protocol, It is a protocol used on TCP/IP networks to automatically assign an IP address to a computer from a pool of available addresses.
 
-
-
 ===========setup default VPC===================
 - from vpc dashboard, we create vpc, IPv4 CIDRs 10.16.0.0/16, IPv6 CIDR amazon managed.
 - 1st subnet: from vpc dashboard, we create subnet, select the vpc we created. subnet name sn-reserved-A, us-east-1a, 
 10.16.0.0/20.  Select IpV6 /56 and type 00 for AZ (A).
-- 2nd subnet: click add new subnet at the bottom, add sn-db-A, us-east-1a, 10.16.16.0/20, IpV6 /56 and type 01 for AZ (A) as well.  Do this for app and web. that sets up AZ-1. 
+- other subnets: click add new subnet at the bottom, add sn-db-A, us-east-1a, 10.16.16.0/20, IpV6 /56 and type 01 for AZ (A) as well.  Do this for app and web. that sets up AZ-1. Do the same for the other AZ's (B) and (C) with name anc AZ east b or c.
+For each subnet: select, actions dropdown, edit settings...enable auto asign IPv6 address
+=====
 
 In planning decide early whether to choose default or dedicated tenancy. 
 -Default tenancy is the default, and is the best choice for most workloads.
@@ -72,5 +69,26 @@ In planning decide early whether to choose default or dedicated tenancy.
 -DNS privided by r53 and available on the base IP of the vpc + 2
 ****** enable DnsHostnames is a setting that enables instances DNS Names
 ****** enable DnsSupport is a setting that enables DNS resolution in vpc. if true the DNS r53 option on the base IP +2 is not available. 
+
+================VPC Routing====================
+-Route tables are used to route traffic between subnets and internet gateways.
+-Route tables are associated with subnets.
+-Route tables are stateful, meaning that if a response is sent to an instance, the response will go out the same interface that the request came in on.
+-Every VPC has a router - Hightly available
+-In every subnet.. 'network+1' address
+-Every subnet has a default route table (Main)
+
+-The destinaiton IP of a route is singular, but it could be an ip for a network. The more specific the route is the higher the priority (end number)
+
+================Internet Gateway===================
+-Internet gateways are used to allow communication between instances in a VPC and the internet. 
+-Internet gateways are stateless, meaning that if a response is sent to an instance, the response will go out the internet gateway.
+-Internet gateways are attached to one or zero VPCs (region resilient)
+***** IPv4 Addresses with an internet gateway: 
+for an ec2 example, a record is created which the IGW maintains - it links the instances private ip to the alocated public ip. the instance itself is not configured with that public IP. thats why when you make an ec2 and allocate it a public ipv4 address, Inside the operating system it only sees the private IP address. So:
+-when you create an ec2 instance, the public IPv4 is not configures in the OS.
+-Bastion hosts and Jumpbox are practiclly the same - and instance inside a public subnet, used to manage incoming connections and facilitate access to internal VPC resources. 
+-When you create a NAT gateway, it is created in a public subnet, and it is used to allow instances in a private subnet to connect to the internet.
+[DEMO] we implement an Internet Gateway, Route Tables and Routes within the Animals4life VPC to support the WEB public subnets. Create IGW, a4l-vpc1-igw name, attach it to a4l vpc. Then create a route table, 
 
 */
