496e97d62f52645b4214a8bff2c793af909e2e81
jesse h <jesselevihall@gmail.com>
2022-12-25 11:10:04 -0700

    name types ec2

--- EC2/EC2_basics.ts
+++ EC2/EC2_basics.ts
@@ -10,8 +10,19 @@ ec2 hosts are either shared or dedicated
 -ec2 can connect to storage - (EBS) which stands for elastic block storage.
 -EBS is also AZ resilient, and lets you allocate volumes of persistent storage that can be alocated to instances in the same AZ
 ============Instance Types=====================
-Instance types names: (family, generation, features, size)
+Instance type names: (family, generation, features, size)
 
+Generally speaking:
+- family: general purpose, compute optimized, memory optimized, storage optimized, accelerator optimized, etc
+- generation: current, previous, etc
+- features: burstable, compute optimized, memory optimized, storage optimized, etc
+- size: size of the instance, small, medium, large, etc
+
+Naming example R5dn.8xlarge (this is an instance type) and = 
+- R5 = family, 5th generation
+- dn = features and additional capabilities
+- 8 = size
+- xlarge = size
 
 
 
