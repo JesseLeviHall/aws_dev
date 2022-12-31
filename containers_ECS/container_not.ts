512a687e1862a5131ca8db5724db3129183c4e57
jesse h <jesselevihall@gmail.com>
2022-12-31 14:02:54 -0700

    container v virtualizations

---
+++ containers_ECS/container_not.ts
@@ -0,0 +1,6 @@
+/* 
+Virtualization means running different OS's on on peice of hardware. 
+Containers on the other hand, are a way to run multiple isolated processes on a single OS instance.
+
+On ec2 for example it would be a waste to run the same OS on mubliple instances and a different app on each instance, just use containers to use one instance and run multiple apps on that instance in seperate environments(containers)
+*/
