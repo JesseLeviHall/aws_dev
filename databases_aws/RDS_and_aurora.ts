3d3e87df4a0c9222399e64e0376924b75ed5766e
jesse h <jesselevihall@gmail.com>
2023-01-06 17:57:09 -0700

    Database migration service

--- databases_aws/RDS_and_aurora.ts
+++ databases_aws/RDS_and_aurora.ts
@@ -1,5 +1,5 @@
 /* 
-===============Databases of AWS====================
+===============RDS and Aurora====================
 Relational Database Service
 DynamoDB - Wide Column database
 Aurora
@@ -96,9 +96,7 @@ read replicas can have their own read replicas, but then lag is a problem, or ca
 In General Snapshots and backups improve RPO but RTO is a problem 
 Read replicas offer near 0 RPO, and very low RTO but good for failure only, not corruption. 
 ===========RDS Security======================
-Authorization
-
-Authentication
+Authorization is handled by IAM, and authentication is handled by the DB engine.
 
 Encyption in Transit - SSL TSL available for rds, can be mandatory. 
 
@@ -115,7 +113,74 @@ Aurora implements a number of radical design changes which offer significant per
 
 Uses a 'cluster', is a single primary instance + 0 or more replicas. These replicas arent like the rds standby ones because they can be used for reads during normal operation, giving the benefits of RDS multi-AZ and RDS read replicas.
 
-No local storage, uses shared cluster volume.
+No local storage, uses shared cluster volumes, spread out in 3 AZs usually a primary, and a few replicas. Much more resilient and less work to manage snapshots and point in time restorations. 
+
+Have up to 15 replicas, any can be failovers, failover will be quick. 
+
+High IOPS low latency all SSD Based. You are billed on consumption high water mark of storage that you use in a cluster. This is being changed and is no longer applicable. 
+
+replicas can be added or removed without provisioning storage
+
+like RDS, DNS endpoints are used to connect to the cluster. The cluster enpoint points to the primary, reader enpoints point to replicas, load balance is automatic. 
+
+There is no free tier, but in comparison to anything beyond RDS single AZ micro, aurora is better value.  
+compute- Billed hourly, per second, 10 minute min. 
+storage - billed monthly, per GB, 10 GB min, IO cost per req
+Free backup alocation. 
+
+Restores will also create a brand new cluster, and manual backups work the same way. 
+
+Aurora has a feature called backtrack, which allows you to restore your database to a point in time within a 1 minute granularity for up to 35 days. 
+
+Fast-Clone makes a new db much faster than a snapshot restore.
+===============aurora serverless==================
+Aurora serverless makes use of ACU (aurora capacity units) which represent a certain amount of compute and corresponding memory. 
+There is a min and max ACU, and the cluster adjusts based on load. Can go to 0 adn can be paused. 
+Consumption billing per-second basis. 
+Same resilience as provisioned(6 copies across AZs). 
+Removes complexity of managing dbs and capacity, maybe more cost effective. 
+Connections are a little more complex for aws - using a proxy fleet managed by aws. they broker the connection to the ACU, so the customer really does little. 
+Good use cases: infrequently used, or new apps, variable or unpredictable workloads, development or tests because no bill for no load and can be paused, or multi-tenant apps. 
+================Database Migration Service(DMS)==============
+AWS Database Migration Service helps you migrate databases to AWS quickly and securely. The source database remains fully operational during the migration, minimizing downtime to applications that rely on the database. The AWS Database Migration Service can migrate your data to and from most widely used commercial and open-source databases.
+- you need to define source and destination endpoints from source db to target db. 
+- One db must be on aws
+- runs using a replication instance on ec2
+Jobs can be one of three types:
+- Full load - copies all data from source to target(if you can afford an outage)
+- Cdc - copies only new or changed data - used when using db native full load export 
+- Full load and cdc - copies all data + copies new or changed data
+If there are schema changes, aws SCT (schema conversion tool) can perform schema conversions. Used ONLY when converting from one db engine to another. 
+You can use DMS in combo with Snowball, which is a petabyte-scale data transport physical solution that uses secure appliances to transfer large amounts of data into and out of AWS. this includes use of SCT. 
+==================AWS Secrets Manager=================
+AWS Secrets manager is a product which can manage secrets within AWS. There is some overlap between it and the SSM Parameter Store - but Secrets manager is specialised for secrets such as passwords, api keys. 
+
+Secrets managed is capable of automatic credential rotation using Lambda.
+
+For supported services it can even adjust the credentials of the service itself.
+
+usable via console, cli, api, or sdk (software dev kit)
+this service is designed to integrate within other aws apps such as RDS, integrates with IAM Permissions. 
+
+Secrets are encrypted at rest with KMS. 
+
+You should default to secrets manager when mention of sectret, rotation, and rds. If mention of anything other than secrets, like config info likely perameter store. 
+
+q: What feature of RDS allows the system to scale for READS
+a: Read replicas
+
+q: Which feature of RDS provides HA Functionality
+a: Multi-AZ
+
+q: How is the standby node of an RDS MultiAZ accessed?
+a: The standby node is accessed only after a failover when it becomes the primary instance. 
+
+q: When restoring RDS from a snapshot or backup is any application reconfiguration required?
+a: A different endpoint address is created when restoring..authentication remains the same. 
+
+q: Which Managed SQL database product in AWS supports 3+ AZ Resilience
+a: Aurora
+
 
 
 
