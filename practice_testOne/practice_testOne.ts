eaa0819df97a777b106b85883b5993f22ba6a8f4
jesse h <jesselevihall@gmail.com>
2023-01-22 19:15:52 -0700

    FIFO Que

--- practice_testOne/practice_testOne.ts
+++ practice_testOne/practice_testOne.ts
@@ -2,7 +2,13 @@
 q: Given a value of 10 WCU which of the statements below is correct? (Choose all that apply)
 a: 10 WCU is the maximum amount of work that can be done in a second
 
-q: 
+q: why would you use a FIFO que
+a: to ensure that the order of the items is maintained
+
+q: Which of the following is not a valid DynamoDB data type?
+a: String
+
+
 
 
 
