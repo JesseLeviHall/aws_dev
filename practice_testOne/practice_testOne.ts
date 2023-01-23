1e47de3fc021a8bafb9af4e1f94c31dd006bdc44
jesse h <jesselevihall@gmail.com>
2023-01-22 19:15:04 -0700

    WCU maximum read

---
+++ practice_testOne/practice_testOne.ts
@@ -0,0 +1,11 @@
+/* 
+q: Given a value of 10 WCU which of the statements below is correct? (Choose all that apply)
+a: 10 WCU is the maximum amount of work that can be done in a second
+
+q: 
+
+
+
+
+
+*/
