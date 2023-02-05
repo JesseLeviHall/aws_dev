a7bd9809e3f67ab820648df0d1de5eda21c57df6
jesse h <jesselevihall@gmail.com>
2023-02-05 11:52:40 -0700

    lambda target group with ALB

--- practice_testOne/practice_testOne.ts
+++ practice_testOne/practice_testOne.ts
@@ -13,4 +13,25 @@ a: to ensure that the message is not processed more than once
 
 Large bills for SQS worker pools - increase RecieveMessagesWaitTimeSeconds. Usually large bills are fro inefficient polling. Short polling pulls only the messages on the queue, long polling is more efficient and less API calls. 
 
+If you want to limit access to s3 bucket with signed urls and you use cloudfront, you have to have an OAI (Origin Access Identity) for cloudfront and a bucket policy that only allows that identity. 
+
+WAF is desinged to 
+- protect web applications from common web exploits that could affect application availability, compromise security, or consume excessive resources, whereas AWS Shield is desined to 
+- protect applications from DDoS attacks.
+
+To get cloudwatch agent to work on an ec2 with no internet connectivity you need to give it an instance role and an Interface endpoint in the VPC. Only DDB and s3 use gateway endpoints. Egress only Internet Gateway is only for IPv6.
+
+To pass a query string perameter into api gateway in the url you use Stage Variables. 
+
+The best practice for Cognito and access to s3 is to apply an Identity Policy to the role defined in Cognito, and use variables to control access. 
+
+Eventually consistent is half teh RCU, and an RCU is 4KB and a WCU is 1KB
+
+Lambda functions should be added to a target group, and the LoadBalancer should point to the target group. 
+
+The VPC and Application resources hae seperate lifecycles, which means defining them in two stacks, generally speaking. Cross stack references allos you to deploy applications stacks wich reference the VPC stack and allow for many application stacks per VPC stack. 
+
+Serveless App Model (SAM) and E beans use cloudformation under the hood. 
+
+
 */
