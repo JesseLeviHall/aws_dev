b0e1ccebc0a23d579166392a0225ac5c2f145632
jesse h <jesselevihall@gmail.com>
2023-02-06 18:23:36 -0700

    Trace MetaData

--- practice_tests/reviewMode1.ts
+++ practice_tests/reviewMode1.ts
@@ -2,6 +2,7 @@
 
 X-ray
 Annotations are simple key-value pairs that are indexed for use with filter expressions. Use annotations to record data that you want to use to group traces in the console, or when calling the GetTraceSummaries API. X-Ray indexes up to 50 annotations per trace.
+-Metadata are key-value pairs with values of any type, including objects and lists, but that are not indexed. Use metadata to record data you want to store in the trace but don’t need to use for searching traces. You can view annotations and metadata in the segment or subsegment details in the X-Ray console.
 
 VPC Flow Logs is a feature that enables you to capture information about the IP traffic going to and from network interfaces in your VPC. Flow log data can be published to Amazon CloudWatch Logs and Amazon S3. After you’ve created a flow log, you can retrieve and view its data in the chosen destination.
 
