2cc9dcb7e6e7fad1b6fc53e65c738c2ee2f38d6e
jesse h <jesselevihall@gmail.com>
2023-02-13 16:56:03 -0700

    local secondary index

---
+++ practice_tests/reviewMode4.ts
@@ -0,0 +1,15 @@
+/* 
+A local secondary index maintains an alternate sort key for a given partition key value. A local secondary index also contains a copy of some or all of the attributes from its base table;
+Strongly consistent reads are not supported on global secondary indexes.
+
+Minimum capacity and maximum capacity are both incorrect because these are the limits that you set for Auto Scaling actions, not for Manual scaling.
+
+
+
+
+
+
+
+
+
+*/
