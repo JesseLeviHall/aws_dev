d870c7af8edf2c77a5623801b16877fca93d0e03
jesse h <jesselevihall@gmail.com>
2023-02-24 15:57:43 -0700

    state machine output

--- practice_tests/reveiwMode5.ts
+++ practice_tests/reveiwMode5.ts
@@ -3,8 +3,20 @@ Test 5
 You can use Amazon Cognito to deliver temporary, limited-privilege credentials to your application so that your users can access AWS resources. Amazon Cognito identity pools support both authenticated and unauthenticated identities. You can retrieve a unique Amazon Cognito identifier (identity ID) for your end-user immediately if you’re allowing unauthenticated users or after you’ve set the login tokens in the credentials provider if you’re authenticating users.
 That is why the correct answer for this question is: Cognito ID.
 
-
-
+The output of a state can be a copy of its input, the result it produces (for example, the output from a Task state’s Lambda function), or a combination of its input and result. Use ResultPath to control which combination of these is passed to the state output.
+You can use a Catch field to capture the error in a Task and Parallel State. This field’s value must be an array of objects, known as catchers.
+A catcher contains the following fields:
+ErrorEquals – A non-empty array of strings that match error names.
+Next – A string that must exactly match one of the state machine’s state names.
+ResultPath – A path that determines what input is sent to the state specified in the Next field.
+When a state reports an error and either there is no Retry field, or if retries fail to resolve the error, Step Functions scans through the catchers in the order listed in the array. When the error name appears in the value of a catcher’s ErrorEquals field, the state machine transitions to the state named in the Next field.
+The four states that handle the application logic and error handling are as follows:
+Choice State – “Yes or No”
+Task State – “YesMessage” and “NoMessage”
+Pass State – “Cause Of Error”
+The workflow is initiated by passing an input of values “yes” or “no”. On the left side, we can see that an error has occurred during the “NoMessage” task state (as labeled by its orange color) when a “no” value was passed as an input.
+On the right side, we can see that all data that passes through the nodes (input, error, output) are aggregated in a single step output.
+This can be done by including a Catch field in the state machine definition to capture the error in a state and the ResultPath to include each node’s input with its output.
 
 
 
