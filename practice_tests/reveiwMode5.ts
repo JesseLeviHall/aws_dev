9968d3b145b44cdc30cc2b3eb6fe482eb228caa4
jesse h <jesselevihall@gmail.com>
2023-02-24 15:54:18 -0700

    start set 5

---
+++ practice_tests/reveiwMode5.ts
@@ -0,0 +1,11 @@
+/* 
+Test 5
+You can use Amazon Cognito to deliver temporary, limited-privilege credentials to your application so that your users can access AWS resources. Amazon Cognito identity pools support both authenticated and unauthenticated identities. You can retrieve a unique Amazon Cognito identifier (identity ID) for your end-user immediately if you’re allowing unauthenticated users or after you’ve set the login tokens in the credentials provider if you’re authenticating users.
+That is why the correct answer for this question is: Cognito ID.
+
+
+
+
+
+
+*/
